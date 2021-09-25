cdfa5b7627fa2b7274b443f22a5bc16b7b2b168e
alex <alxdev94@gmail.com>
2021-09-26 00:34:22 +0100

    document is not available during server side rendering.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,20 @@ import fetch from "node-fetch";
 const sideChil = (<>side bar child her</>)
 
 export default function index() {
+    let url = "";
 
-    	fetch(document.location.href+"/api/cms")
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+        console.log("DEV")
+        url = "/api/cms"
+    } else {
+        url = "https://alexholmes.me/api/cms"
+    }
+
+    	fetch(url)
 		.then((res) => res.json())
 		.then((result) => {
 			console.log(result)
 		});
-
-
-
 
     return (
         <PageCmp hasSidebar={true} pageTitle="Alex" sideChil={sideChil}>
